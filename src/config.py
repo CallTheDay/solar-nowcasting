9dbd84f61da9a8d02ae6589b45fa019dc4b27993
Lohninger <tim.lohninger@liwest.at>
2026-06-10 17:37:35 +0200

    config.py clarification

--- src/config.py
+++ src/config.py
@@ -6,9 +6,9 @@ CAMERA_DIRECTION = 315.0
 # camera sky angle (like 25.0 degrees up from the horizon)
 CAMERA_TILT = 25.0
 
-# coordinates for sun position calculations
-LATITUDE = 48.3064
-LONGITUDE = 14.2858
+# exact geographic coordinates (only needed for sun_position.py which is not yet implemented into the main tracking algorithm)
+LATITUDE = 0
+LONGITUDE = 0
 
 # Frames needed before calculating wind direction
 MIN_FRAMES_REQUIRED = 5
